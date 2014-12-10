8382ae1c3f16dc34c4714c8fe7bf9791246aeefb
Brayden <bb010g@gmail.com>
2014-12-09 17:15:57 -0800

    Merge Game.win{Rows,Columns,Diagonals} into Game.winning

--- src/org/booth/cs2/tictactoe/Game.java
+++ src/org/booth/cs2/tictactoe/Game.java
@@ -28,30 +28,23 @@ public class Game {
 
   public void play() {}
 
-  public static Optional<Piece> winRows(final Board board) {
+  private static Optional<Piece> winning(final Board board) {
     final List<Optional<Piece>> boardList = board.getBoard();
+    // rows
     for (int i = 0; i < 9; i += 3) {
       if (boardList.get(i) == boardList.get(i + 1)
           && boardList.get(i) == boardList.get(i + 2)) {
         return boardList.get(i);
       }
     }
-    return Optional.empty();
-  }
-
-  public static Optional<Piece> winColumns(final Board board) {
-    final List<Optional<Piece>> boardList = board.getBoard();
+    // columns
     for (int i = 0; i < 3; i++) {
       if (boardList.get(i) == boardList.get(i + 3)
           && boardList.get(i) == boardList.get(i + 6)) {
         return boardList.get(i);
       }
     }
-    return Optional.empty();
-  }
-
-  public static Optional<Piece> winDiagonals(final Board board) {
-    final List<Optional<Piece>> boardList = board.getBoard();
+    // diagonals
     if (boardList.get(0) == boardList.get(4)
         && boardList.get(0) == boardList.get(8)
         || boardList.get(2) == boardList.get(4)
